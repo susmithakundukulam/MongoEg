bc22607a785aae713326110c4e6dfc974f86181c
Susmitha <susmithakundukulam@gmail.com>
2021-06-04 13:13:20 +0530

    commented test connection details

--- src/test/java/com/mongoexample/mongo/test/TestMain.java
+++ src/test/java/com/mongoexample/mongo/test/TestMain.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 import org.bson.Document;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+//import com.mongodb.MongoClient;
+//import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -15,17 +15,17 @@ import com.mongodb.client.MongoDatabase;
 public class TestMain {
 	public static void main(String[] args) {
 
-		MongoClientURI uri = new MongoClientURI("mongodb://Susmitha:qwertyqaz@cluster0-shard-00-00.fbies.mongodb.net:27017,cluster0-shard-00-01.fbies.mongodb.net:27017,cluster0-shard-00-02.fbies.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-su4su5-shard-0&authSource=admin&retryWrites=true&w=majority");
-		MongoClient mongoClient = new MongoClient(uri);
-		MongoDatabase database = mongoClient.getDatabase("myDb");
-		 MongoCollection<Document> collection = database.getCollection("books");
-	      //Retrieving the documents
-	      FindIterable<Document> iterDoc = collection.find();
-	      Iterator it = iterDoc.iterator();
-	      while (it.hasNext()) {
-	         System.out.println(it.next());
-	      }
-			      System.out.println("Documents Inserted");
+//		MongoClientURI uri = new MongoClientURI("mongodb://Susmitha:qwertyqaz@cluster0-shard-00-00.fbies.mongodb.net:27017,cluster0-shard-00-01.fbies.mongodb.net:27017,cluster0-shard-00-02.fbies.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-su4su5-shard-0&authSource=admin&retryWrites=true&w=majority");
+//		MongoClient mongoClient = new MongoClient(uri);
+//		MongoDatabase database = mongoClient.getDatabase("myDb");
+//		 MongoCollection<Document> collection = database.getCollection("books");
+//	      //Retrieving the documents
+//	      FindIterable<Document> iterDoc = collection.find();
+//	      Iterator it = iterDoc.iterator();
+//	      while (it.hasNext()) {
+//	         System.out.println(it.next());
+//	      }
+//			      System.out.println("Documents Inserted");
 //		     
 	}
 }
